c6d6c20ad796d8486f82e7335c4c7397bd874233
UNCode101 <18457369+UNCode101@users.noreply.github.com>
2024-06-17 22:23:16 +0000

    removed old component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MoviesComponent } from './movies/movies.component';
 import { SidenavComponent } from './nav/sidenav/sidenav.component';
 import { TopnavComponent } from './nav/topnav/topnav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopnavComponent, SidenavComponent, MoviesComponent, HttpClientModule],
+  imports: [RouterOutlet, TopnavComponent, SidenavComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
