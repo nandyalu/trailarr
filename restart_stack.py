55af978978d6170bc96c4656f7b84d62f3bd01a3
UNCode101 <18457369+UNCode101@users.noreply.github.com>
2024-07-21 04:21:23 +0000

    changed restart stack to just restart instead of stop and start

--- restart_stack.py
+++ restart_stack.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -8,27 +9,74 @@ stack_id = 45
 base_url = "http://10.0.10.131:9000/api"
 stacks_url = base_url + f"/stacks/{stack_id}"
 
-stop_res = requests.post(
-    stacks_url + "/stop", headers={"X-API-KEY": api_key}, params={"endpointId": 1}
-)
+# Get stack file content
+stack_file = requests.get(stacks_url + "/file", headers={"X-API-KEY": api_key})
 try:
-    stop_res.raise_for_status()
-    # print(stop_res.json())
-except requests.exceptions.HTTPError as e:
+    stack_file.raise_for_status()
+    json_file = stack_file.json()
+except Exception as e:
     print("Error: " + str(e))
-    print(stop_res.json())
+    print(stack_file.json())
     exit(1)
 
-start_res = requests.post(
-    stacks_url + "/start", headers={"X-API-KEY": api_key}, params={"endpointId": 1}
+stack_file_content = json_file["StackFileContent"]
+
+# Get Stack details
+stack_details_res = requests.get(stacks_url, headers={"X-API-KEY": api_key})
+try:
+    stack_details_res.raise_for_status()
+    stack_details = stack_details_res.json()
+except Exception as e:
+    print("Error: " + str(e))
+    print(stack_details_res.json())
+    exit(1)
+stack_env = stack_details["Env"]
+stack_webhook = stack_details["Webhook"]
+
+# Now update the stack
+update_data = {
+    "stackFileContent": stack_file_content,
+    "env": stack_env,
+    "prune": False,
+    "webhook": stack_webhook,
+    "pullImage": False,
+}
+update_json = json.dumps(update_data)  # Convert to JSON
+api_response = requests.put(
+    stacks_url,
+    headers={"X-API-KEY": api_key},
+    params={"endpointId": 1},
+    data=update_json,
 )
 try:
-    start_res.raise_for_status()
-    # print(start_res.json())
-except requests.exceptions.HTTPError as e:
+    api_response.raise_for_status()
+    # print(api_response.json())
+except Exception as e:
     print("Error: " + str(e))
-    print(start_res.json())
+    print(api_response.json())
     exit(1)
+
+# stop_res = requests.post(
+#     stacks_url + "/stop", headers={"X-API-KEY": api_key}, params={"endpointId": 1}
+# )
+# try:
+#     stop_res.raise_for_status()
+#     # print(stop_res.json())
+# except requests.exceptions.HTTPError as e:
+#     print("Error: " + str(e))
+#     print(stop_res.json())
+#     exit(1)
+
+# start_res = requests.post(
+#     stacks_url + "/start", headers={"X-API-KEY": api_key}, params={"endpointId": 1}
+# )
+# try:
+#     start_res.raise_for_status()
+#     # print(start_res.json())
+# except requests.exceptions.HTTPError as e:
+#     print("Error: " + str(e))
+#     print(start_res.json())
+#     exit(1)
 
 print("Stack restarted successfully")
 exit(0)
