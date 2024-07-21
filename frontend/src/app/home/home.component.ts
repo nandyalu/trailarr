095e6628e08159639692fa86697c1d1329c5de16
nandyalu <18457369+nandyalu@users.noreply.github.com>
2024-07-21 04:20:38 +0000

    Home page added

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,30 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Media } from '../models/media';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, FormsModule, NgIf, NgFor, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  media_list: Media[] = [];
+  isLoading = true;
 
+  constructor(
+    private movieService: MovieService,
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.movieService.getRecentlyDownloaded().subscribe((media_list: Media[]) => {
+      this.media_list = media_list;
+      this.isLoading = false;
+    });
+  }
 }
