913b818fe4fd5c9ca147e60169810b671176a700
nandyalu <18457369+nandyalu@users.noreply.github.com>
2024-07-26 20:46:00 +0000

    ADD: Session inactivity timer to close websocket connection

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { SidenavComponent } from './nav/sidenav/sidenav.component';
@@ -15,14 +15,20 @@ import { MessageData, WebsocketService } from './services/websocket.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Trailarr';
   messages: MessageData[] = [];
   private toastSubscription?: Subscription;
   private websocketSubscription?: Subscription;
 
+  private idleTime: number = 0;
+  private timeoutId: any;
+  private readonly IDLE_LIMIT: number = 2 * 60 * 1000; // 2 minutes in milliseconds
+
+
   constructor(private websocketService: WebsocketService) { }
 
   ngOnInit() {
+    // Reset the idle timer
+    this.resetIdleTimer();
     // Subscribe to the WebSocket events and display the messages
     this.websocketSubscription = this.websocketService.connect().subscribe({
       next: (data: MessageData) => {
@@ -50,11 +56,34 @@ export class AppComponent {
     });
   }
 
-  ngOnDestroy() {
+  // Uncomment the below code to enable mouse movement detection too!
+  // @HostListener('document:mousemove', ['$event']) 
+  @HostListener('document:click', ['$event'])
+  @HostListener('document:keypress', ['$event'])
+  resetIdleTimer(): void {
+    // Activity detected, reset the idle timer
+    clearTimeout(this.timeoutId);
+    this.idleTime = 0;
+    this.timeoutId = setTimeout(() => {
+      this.closeAllSubscriptions();
+    }, this.IDLE_LIMIT);
+  }
+
+  closeAllSubscriptions() {
+    console.log('Session Idle, closing all subscriptions!');
+    // TODO: May be display a dialog saying session was idle so closed it
+    // and show a button to reload the page
     // Close the websocket connection
     this.websocketService.close();
     // Unsubscribe from the websocket and toast messages
     this.websocketSubscription?.unsubscribe();
     this.toastSubscription?.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    this.closeAllSubscriptions();
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
   }
 }
