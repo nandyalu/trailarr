88d58039598445de72ed50984d2b6d13dddba822
nandyalu <18457369+nandyalu@users.noreply.github.com>
2024-08-07 21:09:33 +0000

    FIX: Issue with PUID/PGID when they already exist inside container, updated logic to use existing user/group if already exists.
    Signed-off-by: nandyalu <18457369+nandyalu@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,23 +19,37 @@ chmod -R 755 /app/assets
 # Set default values for PUID and PGID if not provided
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
-APPUSER=${APPUSER:-appuser}
+APPUSER=appuser
+APPGROUP=appuser
 
-# Create the appuser group and user
-echo "Creating user and group '$APPUSER' with UID '$PUID' and GID '$PGID'"
-if ! getent group "$APPUSER" > /dev/null 2>&1; then
-    groupadd -g "$PGID" "$APPUSER"
+# Create the appuser group and user if they don't exist
+# Check if a group with the supplied PGID already exists
+if getent group "$PGID" > /dev/null 2>&1; then
+    # Use the existing group name
+    APPGROUP=$(getent group "$PGID" | cut -d: -f1)
+    echo "Group with GID '$PGID' already exists, using group '$APPGROUP'"
+else
+    # Create the appuser group if it doesn't exist
+    echo "Creating group '$APPGROUP' with GID '$PGID'"
+    groupadd -g "$PGID" "$APPGROUP"
 fi
 
-if ! id -u "$APPUSER" > /dev/null 2>&1; then
-    useradd -u "$PUID" -g "$APPUSER" -m "$APPUSER"
+# Check if a user with the supplied PUID already exists
+if getent passwd "$PUID" > /dev/null 2>&1; then
+    # Use the existing user name
+    APPUSER=$(getent passwd "$PUID" | cut -d: -f1)
+    echo "User with UID '$PUID' already exists, using user '$APPUSER'"
+else
+    # Create the appuser user if it doesn't exist
+    echo "Creating user '$APPUSER' with UID '$PUID'"
+    useradd -u "$PUID" -g "$PGID" -m "$APPUSER"
 fi
 
 # Set permissions for appuser on /app and /data directories
 echo "Changing the owner of app and data directories to '$APPUSER'"
 chmod -R 750 /app
-chown -R "$APPUSER":"$APPUSER" /app
-chown -R "$APPUSER":"$APPUSER" /data
+chown -R "$APPUSER":"$APPGROUP" /app
+chown -R "$APPUSER":"$APPGROUP" /data
 
 # Switch to the non-root user and execute the command
 echo "Switching to user '$APPUSER' and starting the application"
