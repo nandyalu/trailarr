46eabfe1d08a898e6608a580e5f3538cfa725036
nandyalu <18457369+nandyalu@users.noreply.github.com>
2024-08-10 00:59:15 +0000

    UPD: startup script to backup database before running migrations

--- start.sh
+++ start.sh
@@ -5,9 +5,22 @@
 echo "Running application as user: $(whoami)"
 
 # Run Alembic migrations
+echo "Backing up database before running migrations..."
+new_db=/data/backups/trailarr_$(date +%Y%m%d%H%M%S).db
+old_db=/data/trailarr.db
+mkdir -p /data/backups && cp $old_db $new_db && echo "Database backup created successfully!"
+
+
 echo "Running database migrations with Alembic"
 cd /app/backend
-alembic upgrade head && echo "Database migrations ran successfully!"
+if alembic upgrade head; then
+    echo "Database migrations ran successfully!"
+else
+    echo "Database migrations failed! Restoring backup!"
+    cp $new_db $old_db
+    echo "Backup restored successfully! Sleeping indefinitely to prevent container exit"
+    while :; do sleep 10000000; done
+fi
 
 # Start FastAPI application
 echo "Starting Trailarr application"
