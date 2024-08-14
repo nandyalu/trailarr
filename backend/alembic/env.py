db754d9c6db2319e765d4dcb0fe2e61b9fc41eef
nandyalu <18457369+nandyalu@users.noreply.github.com>
2024-08-14 14:10:39 -0400

    UPD: Minor updates to alembic configuration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -47,6 +47,7 @@ def run_migrations_offline() -> None:
     context.configure(
         url=url,
         target_metadata=target_metadata,
+        render_as_batch=True,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
@@ -69,7 +70,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata, render_as_batch=True
+        )
 
         with context.begin_transaction():
             context.run_migrations()
--- backend/alembic/script.py.mako
+++ backend/alembic/script.py.mako
@@ -10,6 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 import sqlmodel
+import sqlmodel.sql.sqltypes
 ${imports if imports else ""}
 
 # revision identifiers, used by Alembic.
