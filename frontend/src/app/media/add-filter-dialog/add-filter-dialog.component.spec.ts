e35a9947587b8a26959029d16592f5663b174e5a
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-03-05 06:29:32 -0500

    ADD: Add Custom Filter Dialog

---
+++ frontend/src/app/media/add-filter-dialog/add-filter-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddCustomFilterDialogComponent } from './add-filter-dialog.component';
+
+describe('AddFilterDialogComponent', () => {
+  let component: AddCustomFilterDialogComponent;
+  let fixture: ComponentFixture<AddCustomFilterDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddCustomFilterDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddCustomFilterDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/media/add-filter-dialog/add-filter-dialog.component.ts
@@ -0,0 +1,247 @@
+import { Component, computed, EventEmitter, inject, input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import {
+  BooleanFilterCondition,
+  booleanFilterKeys,
+  CustomFilterCreate,
+  DateFilterCondition,
+  dateFilterKeys,
+  FilterCreate,
+  FilterType,
+  NumberFilterCondition,
+  numberFilterKeys,
+  StringFilterCondition,
+  stringFilterKeys
+} from '../../models/customfilter'; // adjust the path as necessary
+import { Media } from '../../models/media';
+import { CustomfilterService } from '../../services/customfilter.service';
+
+@Component({
+  selector: 'app-add-filter-dialog',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './add-filter-dialog.component.html',
+  styleUrl: './add-filter-dialog.component.css'
+})
+export class AddCustomFilterDialogComponent implements OnInit {
+  filterType = input.required<string>();
+  filterTypeValue = computed(() => {
+    let _filterType = this.filterType().toUpperCase();
+    if (_filterType == 'MOVIES') {
+      return FilterType.MOVIES;
+    }
+    if (_filterType == 'SERIES') {
+      return FilterType.SERIES;
+    }
+    if (_filterType == 'TRAILER') {
+      return FilterType.TRAILER;
+    }
+    return FilterType.HOME;
+  });
+  // If an existing custom filter is provided, the form will be initialized for updating;
+  // otherwise it is assumed you are creating a new one.
+  customFilter = input<CustomFilterCreate | null>(null);
+
+  @Output() dialogClosed = new EventEmitter<void>();
+
+  isLoading = true;
+
+  customfilterService = inject(CustomfilterService);
+
+  customFilterForm!: FormGroup;
+
+  computedSignal = computed(() => {
+    let customFilterData = this.customFilter();
+    this.initForm(customFilterData);
+    return true;
+  });
+
+  // Get all enum values for select options.
+  boolFilterConditions = Object.values(BooleanFilterCondition);
+  dateFilterConditions = Object.values(DateFilterCondition);
+  numberFilterConditions = Object.values(NumberFilterCondition);
+  stringFilterConditions = Object.values(StringFilterCondition);
+  filterConditions: string[][] = [] as string[][];
+  filterValueTypes: string[] = [];
+  // Get all filter keys and sort them.
+  filterKeys = booleanFilterKeys.concat(dateFilterKeys, numberFilterKeys, stringFilterKeys).sort();
+
+  viewForOptions = Object.values(FilterType);
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm(this.customFilter());
+  }
+
+  // @ViewChild('customFilterDialog') customFilterDialog!: ElementRef<HTMLDialogElement>;
+  closeDialog(): void {
+    // this.customFilterDialog.nativeElement.close();
+    this.dialogClosed.emit();
+    // setTimeout(() => {
+    // }, 2000);
+    this.filters.clear();
+    this.addFilter();
+    this.customFilterForm.reset();
+  }
+
+  displayTitle(value: string): string {
+    return value.toLowerCase().replace(/_/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
+  }
+
+  initForm(customFilterData: CustomFilterCreate | null): FormGroup {
+    // Initialize the form.
+    let _form = this.fb.group({
+      id: [customFilterData ? customFilterData.id : null],
+      filter_type: [customFilterData ? customFilterData.filter_type : this.filterTypeValue(), Validators.required],
+      filter_name: [customFilterData ? customFilterData.filter_name : '', [Validators.required, Validators.maxLength(15)]],
+      filters: this.fb.array([], Validators.required) // Will hold an array of filters.
+    });
+    this.customFilterForm = _form;
+
+    // If updating an existing CustomFilter, populate the filters FormArray.
+    if (customFilterData && customFilterData.filters && customFilterData.filters.length) {
+      customFilterData.filters.forEach(filter => {
+        this.addFilter(filter);
+      });
+    } else {
+      // Otherwise, initialize with one empty filter.
+      this.addFilter();
+    }
+    this.isLoading = false;
+    return _form;
+  }
+
+  // Getter for the filters FormArray.
+  get filters(): FormArray {
+    return this.customFilterForm.get('filters') as FormArray;
+  }
+
+  // Creates a FormGroup for a single filter.
+  createFilter(filter?: FilterCreate): FormGroup {
+    return this.fb.group({
+      id: [filter ? filter.id : null],
+      filter_by: [filter ? filter.filter_by : '', Validators.required],
+      filter_condition: [filter ? filter.filter_condition : '', Validators.required],
+      filter_value: [filter ? filter.filter_value : '', Validators.required],
+      customfilter_id: [filter ? filter.customfilter_id : null]
+    });
+  }
+
+  // Get the filter conditions for a given filter key.
+  getFilterConditions(filterKey: keyof Media): string[] {
+    if (booleanFilterKeys.includes(filterKey)) {
+      return this.boolFilterConditions;
+    } else if (dateFilterKeys.includes(filterKey)) {
+      return this.dateFilterConditions;
+    } else if (numberFilterKeys.includes(filterKey)) {
+      return this.numberFilterConditions;
+    } else if (stringFilterKeys.includes(filterKey)) {
+      return this.stringFilterConditions;
+    }
+    return [];
+  }
+
+  // Get the filter value type for a given filter key.
+  getFilterValueType(filterKey: keyof Media, filterCondition: string): string {
+    if (booleanFilterKeys.includes(filterKey)) {
+      return 'boolean';
+    } else if (dateFilterKeys.includes(filterKey)) {
+      if (filterCondition === DateFilterCondition.IN_THE_LAST
+        || filterCondition === DateFilterCondition.NOT_IN_THE_LAST) {
+        return 'number_days';
+      }
+      return 'date';
+    } else if (numberFilterKeys.includes(filterKey)) {
+      return 'number';
+    }
+    return 'string';
+  }
+
+  // Adds a new filter FormGroup to the filters FormArray.
+  addFilter(filter?: FilterCreate): void {
+    if (filter && filter.filter_by) {
+      // If a filter is provided, set the filter_condition control to required.
+      this.filterConditions.push(this.getFilterConditions(filter.filter_by));
+      this.filterValueTypes.push(this.getFilterValueType(filter.filter_by, filter.filter_condition));
+    } else {
+      this.filterConditions.push([]);
+      this.filterValueTypes.push('string');
+    }
+    this.filters.push(this.createFilter(filter));
+  }
+
+  // Removes a filter FormGroup from the filters FormArray.
+  removeFilter(index: number): void {
+    this.filterConditions.splice(index, 1);
+    this.filters.removeAt(index);
+  }
+
+  // Add a value change listener for the filter_by control.
+  onFilterByChange(event: Event, index: number): void {
+    // Get the filter_by control.
+    const filterByControl = this.filters.at(index).get('filter_by')?.value as keyof Media;
+    // Update the filter_conditions array for this filter.
+    this.filterConditions[index] = this.getFilterConditions(filterByControl);
+    // Update the filter_value_type for this filter.
+    this.filterValueTypes[index] = this.getFilterValueType(filterByControl, '');
+    // Reset the filter_condition and filter_value controls.
+    this.filters.at(index).get('filter_condition')?.setValue('');
+    this.filters.at(index).get('filter_value')?.reset();
+  }
+
+  onFilterConditionChange(event: Event, index: number): void {
+    // Get the filter_by control.
+    const filterByControl = this.filters.at(index).get('filter_by')?.value as keyof Media;
+    // Set the filter_value_type based on selected condition
+    // Sets to number if the condition is IN_THE_LAST or NOT_IN_THE_LAST.
+    if (event.target) {
+      const filterCondition = (event.target as HTMLSelectElement).value;
+      this.filterValueTypes[index] = this.getFilterValueType(filterByControl, filterCondition);
+    }
+    // Reset the filter_value control.
+    this.filters.at(index).get('filter_value')?.reset();
+  }
+
+  // Called when the form is submitted.
+  onSubmit(): void {
+    if (this.customFilterForm.valid) {
+      // Here you could call a service to save the data.
+      console.log('Submitted Custom Filter:', this.customFilterForm.value);
+      if (this.customFilter()?.id) {
+        // If id exists, update it
+        this.customfilterService.update(this.customFilterForm.value).subscribe(() => {
+          this.closeDialog();
+        });
+      } else {
+        // Otherwise, create a new CustomFilter.
+        this.customfilterService.create(this.customFilterForm.value).subscribe(() => {
+          this.closeDialog();
+        });
+      }
+    } else {
+      // Debugging: Log all invalid form controls.
+      // for (const key of Object.keys(this.customFilterForm.controls)) {
+      //   const control = this.customFilterForm.get(key);
+      //   if (control?.invalid) {
+      //     console.log(`F: ${key} is invalid: ${control}`);
+      //   }
+      //   if (control instanceof FormGroup || control instanceof FormArray) {
+      //     for (const innerKey of Object.keys(control.controls)) {
+      //       const innerControl = control.get(innerKey);
+      //       if (innerControl?.invalid) {
+      //         console.log(`Inner: ${key}.${innerKey} is invalid: ${innerControl}`);
+      //       }
+      //     }
+      //   } else {
+      //     if (control?.invalid) {
+      //       console.log(`${key} is invalid: ${control}`);
+      //     }
+      //   }
+      // }
+      console.log('Form is invalid');
+      // Optionally mark all fields as touched to show validation errors.
+      this.customFilterForm.markAllAsTouched();
+    }
+  }
+}
