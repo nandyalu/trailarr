6e19d41bf82ae21b4bd5681befc8dc06b390e022
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-04-24 08:25:35 -0400

    ADD: Preloading all routes to prevent lagging during navigation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,14 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import {provideRouter, withComponentInputBinding} from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
-import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
-import {provideHttpClient} from '@angular/common/http';
-import {TimeagoModule} from 'ngx-timeago';
-import {routes} from './app.routes';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { TimeagoModule } from 'ngx-timeago';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
     provideHttpClient(),
     // withInterceptors([authInterceptor]),
     {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: {dateFormat: 'medium', timezone: 'UTC'}},
