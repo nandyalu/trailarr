dac2e16954fce732e5e329338282c35fe96be5ef
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-04-24 08:24:02 -0400

    Remove ChangeDetectionStrategy OnPush

--- frontend/src/app/settings/connections/connections.component.ts
+++ frontend/src/app/settings/connections/connections.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-connections',
   imports: [RouterOutlet],
   templateUrl: './connections.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConnectionsComponent {}
--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,13 +1,13 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {RouteAbout, RouteConnections, RouteTrailer} from 'src/routing';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouteAbout, RouteConnections, RouteTrailer } from 'src/routing';
 
 @Component({
   selector: 'app-settings',
   imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent {
   protected readonly RouteAbout = RouteAbout;
