8604398a5d2457b4097929ea900cd2d1dfb7c83a
Alex Rempel <alexey.rempel@googlemail.com>
2025-05-06 11:48:10 +0200

    use generated service, refactor a bit

--- frontend/src/app/services/settings.service.ts
+++ frontend/src/app/services/settings.service.ts
@@ -66,16 +66,6 @@ export class SettingsService {
   }
 
   private connectionsUrl = environment.apiUrl + environment.connections;
-  getConnections(): Observable<Connection[]> {
-    return this.http.get<Connection[]>(this.connectionsUrl).pipe(
-      map((connections: any[]) =>
-        connections.map((connection) => ({
-          ...connection,
-          added_at: new Date(connection.added_at),
-        })),
-      ),
-    );
-  }
 
   getConnection(id: number): Observable<Connection> {
     var connectionIdUrl = this.connectionsUrl + id;
@@ -140,23 +130,6 @@ export class SettingsService {
   updateConnection(connection: ConnectionUpdate): Observable<string> {
     var connectionIdUrl = this.connectionsUrl + connection.id;
     return this.http.put<string>(connectionIdUrl, connection).pipe(
-      catchError((error: any) => {
-        let errorMessage = '';
-        if (error.error instanceof ErrorEvent) {
-          // client-side error
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          // server-side error
-          errorMessage = `Error: ${error.status} ${error.error.detail}`;
-        }
-        return of(errorMessage);
-      }),
-    );
-  }
-
-  deleteConnection(id: number): Observable<string> {
-    var connectionIdUrl = this.connectionsUrl + id;
-    return this.http.delete<string>(connectionIdUrl).pipe(
       catchError((error: any) => {
         let errorMessage = '';
         if (error.error instanceof ErrorEvent) {
--- frontend/src/app/settings/connections/show-connections/show-connections.component.ts
+++ frontend/src/app/settings/connections/show-connections/show-connections.component.ts
@@ -1,68 +1,84 @@
-import {DatePipe, Location, NgFor, NgIf, UpperCasePipe} from '@angular/common';
-import {Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component, ElementRef, inject, OnDestroy, signal, viewChild} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {ConnectionRead, ConnectionsService} from 'generated-sources/openapi';
+import {catchError, of, shareReplay, startWith, Subject, switchMap, tap} from 'rxjs';
+import {LoadIndicatorComponent} from 'src/app/shared/load-indicator';
 import {RouteAdd, RouteConnections, RouteEdit, RouteSettings} from 'src/routing';
-import {Connection} from '../../../models/connection';
-import {SettingsService} from '../../../services/settings.service';
 
 @Component({
   selector: 'app-show-connections',
-  imports: [DatePipe, NgIf, NgFor, RouterLink, UpperCasePipe],
   templateUrl: './show-connections.component.html',
   styleUrl: './show-connections.component.scss',
+  imports: [CommonModule, LoadIndicatorComponent, RouterLink],
 })
-export class ShowConnectionsComponent implements OnInit {
-  private readonly _location = inject(Location);
-  private readonly settingsService = inject(SettingsService);
+export class ShowConnectionsComponent implements OnDestroy {
+  private readonly connectionsService = inject(ConnectionsService);
 
-  connectionList: Connection[] = [];
-  isLoading = false;
+  protected readonly isLoading = signal(true);
+
+  private readonly triggerReload$ = new Subject<void>();
+
   resultMessage = '';
   resultType = '';
+  selectedId = 0;
 
   protected readonly RouteAdd = RouteAdd;
   protected readonly RouteConnections = RouteConnections;
   protected readonly RouteEdit = RouteEdit;
   protected readonly RouteSettings = RouteSettings;
 
-  ngOnInit() {
-    this.getConnections();
+  private readonly dialog = viewChild<ElementRef<HTMLDialogElement>>('deleteConnectionDialog');
+
+  protected readonly connections$ = this.triggerReload$.pipe(
+    startWith('meh'),
+    tap(() => this.isLoading.set(true)),
+    switchMap(() =>
+      this.connectionsService.getConnectionsApiV1ConnectionsGet().pipe(
+        catchError((err) => {
+          console.log('Failed to load connections.', err);
+          return of<ConnectionRead[]>([]);
+        }),
+      ),
+    ),
+    tap(() => this.isLoading.set(false)),
+    shareReplay({refCount: true, bufferSize: 1}),
+  );
+
+  ngOnDestroy() {
+    this.triggerReload$.complete();
   }
 
-  getConnections() {
-    this.isLoading = true;
-    this.settingsService.getConnections().subscribe((connections: Connection[]) => {
-      this.connectionList = connections;
-      this.isLoading = false;
-    });
-  }
+  protected closeDeleteDialog = () => this.dialog()?.nativeElement.close();
+  protected showDeleteDialog = () => this.dialog()?.nativeElement.showModal();
 
-  // Reference to the dialog element
-  @ViewChild('deleteConnectionDialog') deleteConnectionDialog!: ElementRef<HTMLDialogElement>;
-
-  showDeleteDialog(): void {
-    this.deleteConnectionDialog.nativeElement.showModal(); // Open the dialog
-  }
-
-  closeDeleteDialog(): void {
-    this.deleteConnectionDialog.nativeElement.close(); // Close the dialog
-  }
-
-  // showDialog = false;
-  selectedId = 0;
-  onConfirmDelete() {
-    // this.showDialog = false;
+  protected onConfirmDelete() {
     this.closeDeleteDialog();
-    this.settingsService.deleteConnection(this.selectedId).subscribe((res: string) => {
-      this.resultType = 'error';
-      if (res.toLowerCase().includes('success')) {
-        this.resultType = 'success';
-      }
-      this.resultMessage = res;
-      this.getConnections();
-      setTimeout(() => {
-        this.resultMessage = '';
-      }, 3000);
-    });
+    this.connectionsService
+      .deleteConnectionApiV1ConnectionsConnectionIdDelete({connection_id: this.selectedId})
+      .pipe(
+        catchError((error) => {
+          let errorMessage = '';
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error: ${error.status} ${error.error.detail}`;
+          }
+          return of(errorMessage);
+        }),
+      )
+      .subscribe((res) => {
+        this.resultType = 'error';
+        if (res.toLowerCase().includes('success')) {
+          this.resultType = 'success';
+        }
+        this.resultMessage = res;
+        this.triggerReload$.next();
+        setTimeout(() => {
+          this.resultMessage = '';
+        }, 3000);
+      });
   }
 }
