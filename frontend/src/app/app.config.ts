df144e5340c3ca89e48596e0901a2d2531905e75
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-05-09 15:52:08 -0400

    ADD: View Transitions API

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
 import {provideHttpClient} from '@angular/common/http';
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import {PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading} from '@angular/router';
+import {PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading, withViewTransitions} from '@angular/router';
 import {ApiModule} from 'generated-sources/openapi';
 import {TimeagoModule} from 'ngx-timeago';
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules), withViewTransitions()),
     provideHttpClient(),
     // withInterceptors([authInterceptor]),
     {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: {dateFormat: 'medium', timezone: 'UTC'}},
