30cd2bf3ef1b1c9f58546df824d91a3c9e73b52c
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-05-09 15:51:24 -0400

    ADD: test edit-profile page with signals

--- frontend/src/app/settings/profiles/edit-profile/edit-profile.component.ts
+++ frontend/src/app/settings/profiles/edit-profile/edit-profile.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, input} from '@angular/core';
+import {ProfileService} from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-edit-profile',
   imports: [],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.scss'
+  styleUrl: './edit-profile.component.scss',
 })
 export class EditProfileComponent {
+  profileId = input(0, {transform: Number});
+  profileService = inject(ProfileService);
 
+  constructor() {
+    effect(() => {
+      let id = this.profileId();
+      console.log('Profile ID:', id);
+      this.profileService.selectedProfileId.set(id);
+    });
+  }
 }
