ad2e3e6054a2fe0cdd124e1495caddf5a4367337
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-02 19:06:54 -0400

    ADD: Created individual components for text input, options select, and range slider

---
+++ frontend/src/app/settings/profiles/settings/options-setting/options-setting.component.ts
@@ -0,0 +1,24 @@
+import {Component, input, output} from '@angular/core';
+
+@Component({
+  selector: 'app-options-setting',
+  imports: [],
+  templateUrl: './options-setting.component.html',
+  styleUrl: './options-setting.component.scss',
+})
+export class OptionsSettingComponent {
+  // Setting a unique ID for the input element, setting a signal value gives a browser warning
+  // so we generate a random string for the ID
+  readonly inputId = 'options-setting-' + Math.random().toString(36).substring(2, 10);
+
+  name = input.required<string>();
+  description = input<string>('');
+  descriptionExtra = input<string>('');
+  options = input.required<string[]>();
+  selectedOption = input('', {transform: String});
+  optionChange = output<string>();
+
+  onOptionClick(option: string): void {
+    this.optionChange.emit(option);
+  }
+}
---
+++ frontend/src/app/settings/profiles/settings/text-setting/text-setting.component.ts
@@ -0,0 +1,45 @@
+import {Component, input, model, OnInit, output, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-text-setting',
+  imports: [FormsModule],
+  templateUrl: './text-setting.component.html',
+  styleUrl: './text-setting.component.scss',
+})
+export class TextSettingComponent implements OnInit {
+  // Setting a unique ID for the input element, setting a signal value gives a browser warning
+  // so we generate a random string for the ID
+  readonly inputId = 'text-setting-' + Math.random().toString(36).substring(2, 10);
+
+  // Define the inputs for the component
+  name = input.required<string>();
+  description = input<string>('');
+  descriptionExtra = input<string>('');
+  isNumberType = input.required<boolean>();
+  isLongInput = input.required<boolean>();
+  placeholder = input<string>('');
+  value = model.required<string | number>();
+  minLength = input<number>(0);
+  maxLength = input<number>(150);
+  autocomplete = input<string>('off');
+  disabled = input<boolean>(false);
+
+  oldValue = signal<string | number>('');
+  onSubmit = output<string>();
+
+  ngOnInit() {
+    this.oldValue.set(this.value());
+  }
+
+  resetValue(): void {
+    this.value.set(this.oldValue());
+  }
+
+  onSubmitValue(): void {
+    let submitValue = this.value();
+    submitValue = submitValue.toString().trim();
+    this.onSubmit.emit(submitValue);
+    this.oldValue.set(submitValue);
+  }
+}
