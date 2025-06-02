af3a21ff95e86ec64304f03c064885dd082dfca5
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-02 19:35:37 -0400

    UPD: Navigation now resets scroll position to top

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,27 @@
 import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
 import {provideHttpClient} from '@angular/common/http';
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import {PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading, withViewTransitions} from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withPreloading,
+  withViewTransitions,
+} from '@angular/router';
 import {ApiModule} from 'generated-sources/openapi';
 import {TimeagoModule} from 'ngx-timeago';
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules), withViewTransitions()),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules),
+      withViewTransitions(),
+      withInMemoryScrolling({scrollPositionRestoration: 'top', anchorScrolling: 'enabled'}),
+    ),
     provideHttpClient(),
     // withInterceptors([authInterceptor]),
     {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: {dateFormat: 'medium', timezone: 'UTC'}},
