9269eecb875ed4f9713f3512e830c6c7c3cf49f5
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-02 18:43:30 -0400

    ADD: methods for updating profile settings and deleting profile

--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -1,14 +1,16 @@
 import {httpResource} from '@angular/common/http';
-import {computed, Injectable, signal} from '@angular/core';
-import {TrailerProfileRead} from 'generated-sources/openapi';
+import {computed, inject, Injectable, signal} from '@angular/core';
+import {TrailerProfileRead, TrailerProfilesService} from 'generated-sources/openapi';
 import {environment} from 'src/environment';
+import {WebsocketService} from './websocket.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
-  // private httpClient = inject(HttpClient);
   private profilesUrl = environment.apiUrl + environment.trailerprofiles;
+  private _service = inject(TrailerProfilesService);
+  private websocketService = inject(WebsocketService);
 
   readonly allProfiles = httpResource<TrailerProfileRead[]>(this.profilesUrl, {defaultValue: []});
 
@@ -42,4 +44,75 @@ export class ProfileService {
     // If not found, return the value from the fetchSelectedProfileResource
     return this.fetchSelectedProfileResource.value();
   });
+
+  updateSetting(key: keyof TrailerProfileRead, value: any) {
+    const selectedId = this.selectedProfileId();
+    if (selectedId <= 0) {
+      throw new Error('No profile selected');
+    }
+
+    this._service
+      .updateTrailerProfileSettingApiV1TrailerprofilesTrailerprofileIdSettingPost({
+        trailerprofile_id: selectedId,
+        body: {
+          key: key,
+          value: value,
+        },
+      })
+      .subscribe({
+        next: (updatedProfile) => {
+          // Update the local allProfiles signal with the updated profile
+          this.allProfiles.update((profiles) => {
+            const index = profiles.findIndex((profile) => profile.id === updatedProfile.id);
+            if (index !== -1) {
+              profiles[index] = updatedProfile;
+              return [...profiles];
+            }
+            return [...profiles, updatedProfile];
+          });
+          const _key = key.charAt(0).toUpperCase() + key.replaceAll('_', ' ').slice(1);
+          this.websocketService.showToast(`${_key} set to '${value}' successfully!`, 'success');
+        },
+        error: (error) => {
+          // Log the server's error message
+          let errorMessage = 'An unknown error occurred!';
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error: ${error.status} ${error.error.detail}`;
+          }
+          console.error('Failed to update profile setting:', errorMessage);
+          this.websocketService.showToast(errorMessage, 'error');
+        },
+      });
+  }
+
+  deleteProfile(id: number) {
+    if (id <= 0) {
+      throw new Error('Invalid profile ID');
+    }
+    this._service.deleteTrailerProfileApiV1TrailerprofilesTrailerprofileIdDelete({trailerprofile_id: id}).subscribe({
+      next: () => {
+        // Reset the selected profile ID and reload profiles
+        this.selectedProfileId.set(0);
+        this.allProfiles.reload();
+        this.websocketService.showToast('Profile deleted successfully!', 'success');
+      },
+      error: (error) => {
+        // Log the server's error message
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          errorMessage = `Error: ${error.status} ${error.error.detail}`;
+        }
+        console.error('Failed to delete profile:', errorMessage);
+        this.websocketService.showToast(errorMessage, 'error');
+      },
+    });
+  }
 }
