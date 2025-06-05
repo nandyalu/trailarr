64eec7e7daa06f08047409c9f4e1c4da07c4ac92
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-05 04:25:33 -0400

    UPD: Removed settings moved to Profiles and adjusted layout, and use signals

--- frontend/src/app/settings/trailer/trailer.component.ts
+++ frontend/src/app/settings/trailer/trailer.component.ts
@@ -1,96 +1,43 @@
-import {NgFor, NgIf} from '@angular/common';
-import {Component, inject, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {WebsocketService} from 'src/app/services/websocket.service';
 import {LoadIndicatorComponent} from 'src/app/shared/load-indicator';
 import {Settings} from '../../models/settings';
 import {SettingsService} from '../../services/settings.service';
+import {OptionsSettingComponent} from '../profiles/settings/options-setting/options-setting.component';
+import {TextSettingComponent} from '../profiles/settings/text-setting/text-setting.component';
 
 @Component({
   selector: 'app-trailer',
-  imports: [NgIf, NgFor, FormsModule, LoadIndicatorComponent],
+  imports: [FormsModule, LoadIndicatorComponent, OptionsSettingComponent, TextSettingComponent],
   templateUrl: './trailer.component.html',
   styleUrl: './trailer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TrailerComponent implements OnInit {
+export class TrailerComponent {
   private readonly settingsService = inject(SettingsService);
+  private webSocketService = inject(WebsocketService);
 
-  isLoading: boolean = false;
-  settings?: Settings;
-  updateResults: string[] = [];
-  monitorInterval = 60;
-  resolution = 1080;
-  audioVolumeLevel = 100;
-  subtitleLanguage = 'en';
+  protected readonly isLoading = this.settingsService.settingsResource.isLoading;
+  protected readonly settings = this.settingsService.settingsResource.value;
+
   loggingOptions = ['Debug', 'Info', 'Warning', 'Error'];
-  trueFalseOptions = [true, false];
-  fileFormats = ['mkv', 'mp4', 'webm'];
-  audioFormats = ['aac', 'ac3', 'eac3', 'flac', 'opus'];
-  videoFormats = ['h264', 'h265', 'vp8', 'vp9', 'av1'];
-  subtitleFormats = ['srt', 'vtt'];
-  trailerFileName = '';
-  ytCookiesPath = '';
-  excludeWords = '';
-  minDuration = 30;
-  maxDuration = 600;
-  trailerSearchQuery = '';
-  urlBase = '';
+  trueFalseOptions = ['true', 'false'];
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.getSettings();
-  }
-
-  getSettings() {
-    this.settingsService.getSettings().subscribe((settings) => {
-      this.settings = settings;
-      this.monitorInterval = settings.monitor_interval;
-      this.resolution = settings.trailer_resolution;
-      this.subtitleLanguage = settings.trailer_subtitles_language;
-      this.trailerFileName = settings.trailer_file_name;
-      this.ytCookiesPath = settings.yt_cookies_path;
-      this.excludeWords = settings.exclude_words;
-      this.minDuration = settings.trailer_min_duration;
-      this.maxDuration = settings.trailer_max_duration;
-      this.trailerSearchQuery = settings.trailer_search_query;
-      this.urlBase = settings.url_base;
-      this.isLoading = false;
-    });
+  toTitleCase(str: string): string {
+    return str
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
   }
 
   updateSetting(key: keyof Settings, value: any) {
-    // Do not update if setting value hasn't changed
-    if (this.settings ? this.settings[key] === value : false) {
-      return;
-    }
-    // Special handling for trailer_file_format
-    if (key === 'trailer_file_format') {
-      // If the file format is webm, ensure the audio and video formats are compatible
-      let videoCodec = this.settings?.trailer_video_format;
-      if (value.toLowerCase() === 'webm') {
-        if (this.settings?.trailer_audio_format != 'opus') {
-          this.updateSetting('trailer_audio_format', 'opus');
-        }
-        if (videoCodec != 'vp8' && videoCodec != 'vp9' && videoCodec != 'av1') {
-          this.updateSetting('trailer_video_format', 'vp9');
-        }
-      }
-      // If the file format is mp4, ensure the audio and video formats are compatible
-      if (value.toLowerCase() === 'mp4') {
-        if (videoCodec != 'h264' && videoCodec != 'h265' && videoCodec != 'av1') {
-          this.updateSetting('trailer_video_format', 'h265');
-        }
-      }
-    }
     this.settingsService.updateSetting(key, value).subscribe((msg) => {
-      // Add update result message to end of list
-      this.updateResults.push(msg);
+      // Show update result message
+      const status = msg.toLowerCase().includes('error') ? 'Error' : 'Success';
+      this.webSocketService.showToast(msg, status);
       // Update the settings after the change
-      this.getSettings();
-      // Hide the message after 3 seconds
-      setTimeout(() => {
-        // Remove the first message (oldest) from the list
-        this.updateResults.shift();
-      }, 3000);
+      this.settingsService.settingsResource.reload();
     });
   }
 }
