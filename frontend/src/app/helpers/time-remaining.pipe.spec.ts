9a9312250f6132c8243369e8284482ae81b44ca0
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-14 16:22:36 -0400

    ADD: Copy to clipboard directive and Time remaining pipe (async)

---
+++ frontend/src/app/helpers/time-remaining.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeRemainingPipe } from './time-remaining.pipe';
+
+describe('TimeRemainingPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeRemainingPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
