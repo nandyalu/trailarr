0cb6d592fd0b507a7062be5016bebc3995c3df33
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-06-24 03:46:37 -0400

    ADD: Global Error Handler

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
 import {provideHttpClient} from '@angular/common/http';
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig, ErrorHandler, importProvidersFrom} from '@angular/core';
 import {PreloadAllModules, provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading} from '@angular/router';
 import {ApiModule} from 'generated-sources/openapi';
 import {TimeagoModule} from 'ngx-timeago';
 import {routes} from './app.routes';
+import {GlobalErrorHandler} from './error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,7 @@ export const appConfig: ApplicationConfig = {
       // withViewTransitions(),
       withInMemoryScrolling({scrollPositionRestoration: 'top', anchorScrolling: 'enabled'}),
     ),
+    {provide: ErrorHandler, useClass: GlobalErrorHandler},
     provideHttpClient(),
     // withInterceptors([authInterceptor]),
     {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: {dateFormat: 'medium', timezone: 'UTC'}},
