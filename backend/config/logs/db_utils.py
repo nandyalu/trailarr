8fe0e73765710a964f292a30c70a10d9fcc53b66
Uma Nandyala <18457369+nandyalu@users.noreply.github.com>
2025-07-13 02:18:44 -0400

    UPD: Logs in database

--- backend/config/logs/db_utils.py
+++ backend/config/logs/db_utils.py
@@ -1,13 +1,23 @@
-from contextlib import contextmanager
+from contextlib import asynccontextmanager, contextmanager
 from sqlalchemy import text as sa_text
 from sqlmodel import Session, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from config.logs.model import LogBase, AppLogRecord  # noqa F401
 from config.settings import app_settings
 
 logs_db = f"sqlite:///{app_settings.app_data_dir}/logs/logs.db"
+logs_async_db = f"sqlite+aiosqlite:///{app_settings.app_data_dir}/logs/logs.db"
+
 engine = create_engine(
     logs_db,
+    connect_args={"check_same_thread": False},
+    echo=False,
+)
+
+async_engine = create_async_engine(
+    logs_async_db,
     connect_args={"check_same_thread": False},
     echo=False,
 )
@@ -33,3 +43,16 @@ def get_logs_session():
         session.rollback()
     finally:
         session.close()
+
+
+@asynccontextmanager
+async def get_async_logs_session():
+    """Create a new async session for logs database operations."""
+    async with AsyncSession(async_engine) as session:
+        try:
+            yield session
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            await session.rollback()
+        finally:
+            await session.close()
--- backend/config/logs/manager.py
+++ backend/config/logs/manager.py
@@ -1,14 +1,30 @@
-from sqlmodel import select
-from config.logs.db_utils import get_logs_session
-from config.logs.model import AppLogRecord
+from sqlmodel import col, desc, select
+from config.logs.db_utils import get_async_logs_session
+from config.logs.model import AppLogRecord, LOG_LEVELS, LogLevel
 from config.settings import app_settings
 
 
-def get_all_logs():
+async def get_all_logs(
+    level: LogLevel | None = None, offset: int = 0, limit: int = 1000
+) -> list[AppLogRecord]:
     """Retrieve all logs from the database."""
 
-    level = app_settings.log_level
-    with get_logs_session() as session:
-        stmt = select(AppLogRecord).where(AppLogRecord.level == level)
-        logs = session.exec(stmt).all()
-        return logs
+    if level is None:
+        level = LogLevel[app_settings.log_level]
+    async with get_async_logs_session() as session:
+        stmt = select(AppLogRecord)
+        # Get log levels greater than or equal to the specified level
+        _given_val = LOG_LEVELS.get(level.value.upper(), 20)
+        _levels_to_get = [
+            _level
+            for _level, _value in LOG_LEVELS.items()
+            if _value >= _given_val
+        ]
+        stmt = stmt.where(col(AppLogRecord.level).in_(_levels_to_get))
+        stmt = (
+            stmt.offset(offset)
+            .limit(limit)
+            .order_by(desc(AppLogRecord.created))
+        )
+        logs = await session.exec(stmt)
+        return list(logs)
