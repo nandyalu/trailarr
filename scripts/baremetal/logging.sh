855acafdb0bec69136a587b9c0998c4850b71968
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-08-14 16:46:11 -0400

    UPD: More logging changes

--- scripts/baremetal/logging.sh
+++ scripts/baremetal/logging.sh
@@ -123,6 +123,7 @@ start_message() {
 update_progress() {
     if $PROGRESS_ACTIVE; then
         PROGRESS_INDEX=$(( (PROGRESS_INDEX + 1) % ${#PROGRESS_CHARS[@]} ))
+        clear_line
         printf "${PROGRESS_COLOR}%s %s${NC}" "${PROGRESS_CHARS[PROGRESS_INDEX]}" "$PROGRESS_MSG"
     fi
 }
