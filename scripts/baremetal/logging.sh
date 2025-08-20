c46c7ed8635b48207b4907d7e0d2391ae86c445a
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-08-20 19:42:46 -0400

    FIX: Codefactor issues

--- scripts/baremetal/logging.sh
+++ scripts/baremetal/logging.sh
@@ -142,7 +142,7 @@ _parse_status_message() {
     local color_code
     local msg
     local default_color="$3"
-    if [ "$#" -eq 1 ] || ( [ "$#" -eq 2 ] && [ -z "$2" ] ); then
+    if [ "$#" -eq 1 ] || { [ "$#" -eq 2 ] && [ -z "$2" ]; }; then
         color_code="${default_color:-$GREEN}"
         msg="$1"
     else
