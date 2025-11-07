8022c489c2bc369072026897ac9be6ae8245464f
nandyalu <18457369+nandyalu@users.noreply.github.com>
2025-11-07 14:35:54 -0500

    ADD: model conventions for use with migrations

---
+++ backend/alembic/model_conventions.py
@@ -0,0 +1,7 @@
+CONVENTIONS = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "%(table_name)s_pkc",
+}
