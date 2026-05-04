48073ea5a2e5c581cb3605cf9d18dc0f050c8853
nandyalu <18457369+nandyalu@users.noreply.github.com>
2026-05-04 04:16:49 +0000

    upd: remove restrictions on the download button

--- frontend/src/app/media/media-details/media-details.component.ts
+++ frontend/src/app/media/media-details/media-details.component.ts
@@ -152,25 +152,24 @@ export class MediaDetailsComponent {
   }
 
   /**
-   * Downloads the trailer for the current media if the trailer ID has changed.
+   * Downloads the trailer for the current media.
    *
-   * This method checks if the new trailer URL contains the old trailer ID and if the trailer already exists.
-   * If the trailer ID is the same and the trailer exists, it does not proceed with the download.
-   * Otherwise, it sets the loading state to true and initiates the download process via the media service.
+   * This method sets the loading state to true and initiates the download process via the media service.
+   * Once the download is complete, the loading state is set to false.
    *
    * @param {number} profileId - The ID of the profile to use for downloading the trailer.
    *
    * @returns {void}
    */
   downloadTrailer(profileId: number): void {
-    const old_id = this.selectedMedia()?.youtube_trailer_id?.toLowerCase() || '';
-    const new_id = this.trailer_url.toLowerCase();
-    const trailer_exists = this.selectedMedia()?.trailer_exists || false;
-    if (old_id && new_id.includes(old_id) && trailer_exists) {
-      // Trailer id is the same, no need to download
-      this.webSocketService.showToast('Trailer ID is the same as the existing one. No need to download.', 'Error');
-      return;
-    }
+    // const old_id = this.selectedMedia()?.youtube_trailer_id?.toLowerCase() || '';
+    // const new_id = this.trailer_url.toLowerCase();
+    // const trailer_exists = this.selectedMedia()?.trailer_exists || false;
+    // if (old_id && new_id.includes(old_id) && trailer_exists) {
+    //   // Trailer id is the same, no need to download
+    //   this.webSocketService.showToast('Trailer ID is the same as the existing one. No need to download.', 'Error');
+    //   return;
+    // }
     this.isLoadingDownload.set(true);
     // console.log('Downloading trailer');
     this.mediaService.downloadMediaTrailer(this.mediaId(), profileId, this.trailer_url).subscribe((res: string) => {
